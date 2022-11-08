1b06fb493f627562886a009fdf49e915f2eb002e
weijuli <weijuli1810@gmail.com>
2022-11-09 06:45:19 +0800

    Initial Commit

---
+++ v_map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  v_map
+//
+//  Created by weinu on 2022/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ v_map/v_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  v_mapApp.swift
+//  v_map
+//
+//  Created by weinu on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct v_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
